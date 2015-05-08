f86ef9e8f7abb37c0565aeee1b24c85306d635ea
Erik Muttersbach <erik@muttersbach.net>
2015-05-08 09:29:21 +0200

    Fixed cmake module

--- cmake_modules/Findlibhdfs.cmake
+++ cmake_modules/Findlibhdfs.cmake
@@ -2,7 +2,7 @@ if (NOT LIBHDFS_FOUND)
     include(FindPackageHandleStandardArgs)
 
     find_path(LIBHDFS_INCLUDE_DIR hdfs.h PATHS /usr/local/include /usr/include /usr/local/hadoop/include/)
-    find_library(LIBHDFS_LIBRARY hdfs PATHS /usr/local/hadoop/lib/native)
+    find_library(LIBHDFS_LIBRARY hdfs PATHS /usr/local/hadoop/lib/native /usr/lib/hadoop/lib/native/)
 
     find_package_handle_standard_args(LIBHDFS DEFAULT_MSG LIBHDFS_LIBRARY LIBHDFS_INCLUDE_DIR)
 
