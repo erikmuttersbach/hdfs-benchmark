26cfd2550117537af988193482e021234549ab6a
Erik Muttersbach <erik@muttersbach.net>
2015-06-28 21:24:57 +0200

    benchmark.rb

--- scripts/benchmark.rb
+++ scripts/benchmark.rb
@@ -1,4 +1,5 @@
 # Determine best buffer size
+=begin
 [:hot]. each do |h|
 [512].each do |block_size|
 ['2000M'].each do |file|
@@ -16,32 +17,11 @@ end
 end
 end
 end
-
-# Determine best block size
-=begin
-[:hot, :cold]. each do |h|
-['2000M'].each do |file|
-[64*1024].each do |buffer_size|
-[:standard, :scr].each do |read_type|
-puts "#{h}, #{buffer_size} buffer, #{file}, #{read_type}"
-[128, 256, 512].each do |block_size|
-    (1..5).each do |i|
-        `sudo drop_caches` if h == :cold
-        print (`./build/hdfs_reader -f /data/bs#{block_size}/#{file} -b #{buffer_size} -t #{read_type}`).gsub("\n", '').gsub(',', '.')+" "
-    end
-    puts " "
-end
-puts "\n"
-end
-end
-end
-end
 =end
 
 # Show read speed for different files
-=begin
 [:hot, :cold]. each do |h|
-[1024*64].each do |buffer_size|
+[1024*1024].each do |buffer_size|
 [512].each do |block_size|
 [:scr].each do |read_type|
 puts "#{h}, #{buffer_size} buffer, #{read_type}, VS #{block_size}"
@@ -57,7 +37,23 @@ end
 end
 end
 end
-=end
+
+# Determine best block size
+[:hot, :cold]. each do |h|
+['2000M'].each do |file|
+[1024*1024].each do |buffer_size|
+[:standard, :scr, :zcr].each do |read_type|
+[128, 256, 512].each do |block_size|
+    (1..5).each do |i|
+        `sudo drop_caches` if h == :cold
+        print (`./build/hdfs_reader -f /data/bs#{block_size}/#{file} -b #{buffer_size} -t #{read_type}`).gsub("\n", '').gsub(',', '.')+" "
+    end
+    puts " "
+end
+end
+end
+end
+end
 
 # Skip Checksums
 =begin
